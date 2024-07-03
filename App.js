3517586acfd017ef31dcbc7102ef3c5f1726a66f
vyas10 <pratikvyas9875@gmail.com>
2024-07-03 21:22:56 +0530

    message

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", { id: "heading" }, "Hello React using React"); // This is an object as a whole and not a tag
+const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "This is h1"), React.createElement("h2", {}, "This is h2")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "This is h1"), React.createElement("h2", {}, "This is h2")])]) // This is pretty messed up so to make it easier, jsx is been used but we saw this as this is core React and that is what it means to us.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render([heading, parent]); // render is responsible to convert the object into the tag and put it on the DOM.
